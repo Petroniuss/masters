a71d2ee05d94a51ce6ad18ff923afab98df9c33b
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-12-28 21:42:22 +0100

    add ethers dependencies

--- organisation/src/main.rs
+++ organisation/src/main.rs
@@ -3,6 +3,30 @@ mod permission_graph;
 fn main() {
     println!("Master's");
 }
+// next step - interact with the smart contract
+// we need to generate rust bindings.
+// let's create a cargo task that does it.
+
+
+
+// next step - interact with IPFS
+
+// Need a way to:
+// - bootstrap IPFS
+// - boostrap the permission graph smart contract
+// - initialize the permission graph
+// - visualize the graph live.
+// would be nice if I could do everything with docker compose
+
+
+
+
+
+// hmm but I need to be able to interact with some pieces - how?
+// the easiest would be to expose some simple API and use curl/postman/scripts?
+// or maybe some dummy front-end - hopefully compile rust to wasm and serve that?
+// trunk/yew seems to be the way to go.
+
 
 // Organisation should be both an HTTP server so that other organisations can communicate with it
 // and be an interactive CLI so that we can play with it easily.
