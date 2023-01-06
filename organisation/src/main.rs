613195a7ddcb8af991d6a96e6e948f20e112e52e
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2023-01-06 16:30:34 +0100

    add a library crate

--- organisation/src/main.rs
+++ organisation/src/main.rs
@@ -2,18 +2,24 @@ mod bindings;
 mod permission_graph;
 mod smart_contract;
 
+/// Goal for today:
+/// Let's now write the off-chain code that interacts with the smart contract
+/// Things that we need:
+/// - deploy multiple smart contracts but initialized with different arguments
+/// - interact with the smart contract:
+///   - propose a change
+///   - validate the change
+/// - update docker-compose to run hello-world setup:
+///   - ideally this would be an **example**
+///
+/// Using examples:
+///   - create lib crate.
+///   - create examples folder.
+///   - create bin crate consuming lib crate.
+///
+/// Stretch:
+/// - off-chain code assembling knowledge from multiple smart contracts,
+/// - that's able to respond to queries whether given operation is allowed.
 fn main() {
     println!("Ala!");
 }
-
-// Goal for today:
-// Let's now write the off-chain code that interacts with the smart contract
-// Things that we need:
-// - deploy multiple smart contracts but initialized with different arguments
-// - interact with the smart contract:
-//   - propose a change
-//   - validate the change
-
-// Stretch:
-// - off-chain code assembling knowledge from multiple smart contracts,
-// - that's able to respond to queries whether given operation is allowed.
